fb5f2413a76627e17a2e7d22899beb444d008414
Justin Clark <jclarksummit@gmail.com>
2023-08-03 15:08:04 -0400

    Initial commit

---
+++ src/Homepage.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import "./Homepage.css";
+
+/** Homepage of app
+ *
+ * Shows welcome message.
+ *
+ * Routed at /
+ *
+ * Routes -> Homepage
+ *
+ */
+
+function Homepage() {
+  return (
+    <div className="Homepage">
+      <h1>Homepage rendered</h1>
+    </div>
+  );
+}
+
+export default Homepage;
