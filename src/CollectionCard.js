ba967b21d315bdcef593da7689482fdbe64cd421
Justin Clark <jclarksummit@gmail.com>
2023-08-03 14:05:23 -0400

    Set up boilerplate for CollectionCard component

--- src/CollectionCard.js
+++ src/CollectionCard.js
@@ -10,12 +10,13 @@ import "./CollectionCard.css";
  * CollectionList -> CollectionCard
  */
 
-function CollectionCard({ id, name, description }) {
+function CollectionCard({ id, name, description, createdAt }) {
   return (
     <a className="CollectionCard" href={`/collections/${id}`}>
       <div className="card-body">
         <h5 className="card-title">{name}</h5>
         <p className="card-text">{description}</p>
+        <p className="card-text"><small className="text-muted">{createdAt}</small></p>
       </div>
     </a>
   );
