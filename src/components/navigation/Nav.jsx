1bc1e5c7aa11cc0246b48fd5ff698263be29654c
Justin Clark <jclarksummit@gmail.com>
2023-08-04 18:13:37 -0400

    Add basic navbar w/ shadcn-ui

--- src/components/navigation/Nav.jsx
+++ src/components/navigation/Nav.jsx
@@ -1,5 +1,16 @@
 import { Link, NavLink } from 'react-router-dom';
 import { useContext } from 'react';
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  NavigationMenuViewport,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu";
 
 /** Nav bar for app that shows on every page.
  *
@@ -10,26 +21,36 @@ import { useContext } from 'react';
 
 function Nav() {
   return (
-    <nav className="Nav">
-      <ul className="Navbar">
-        <li className="nav-item">
-          <NavLink className="nav-link" to="/">
-            Home
-          </NavLink>
-          <NavLink className="nav-link" to="/scrape-data">
-            Scrape Data
-          </NavLink>
-          <NavLink className="nav-link" to="/collections">
-            My Collections
-          </NavLink>
-          <NavLink className="nav-link" to="/about">
-            About
-          </NavLink>
-          <NavLink className="settings" to="/settings">
-            Settings
-          </NavLink>
-        </li>
-      </ul>
+    <nav className="Nav flex justify-center mt-10">
+        <NavigationMenu className="border">
+          <NavigationMenuList>
+            <NavigationMenuItem className=''>
+              <NavLink to="/">
+                <NavigationMenuLink className={navigationMenuTriggerStyle()}>Home</NavigationMenuLink>
+              </NavLink>
+            </NavigationMenuItem>
+            <NavigationMenuItem className=''>
+              <NavLink to="/scrape">
+                <NavigationMenuLink className={navigationMenuTriggerStyle()}>Scrape Data</NavigationMenuLink>
+              </NavLink>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+              <NavLink to="/collections">
+                <NavigationMenuLink className={navigationMenuTriggerStyle()}>Saved Collections</NavigationMenuLink>
+              </NavLink>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+              <NavLink to="/about">
+                <NavigationMenuLink className={navigationMenuTriggerStyle()}>About</NavigationMenuLink>
+              </NavLink>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+              <NavLink to="/settings">
+                <NavigationMenuLink className={navigationMenuTriggerStyle()}>Settings</NavigationMenuLink>
+              </NavLink>
+            </NavigationMenuItem>
+          </NavigationMenuList>
+        </NavigationMenu>
     </nav>
   );
 }
