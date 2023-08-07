8033326842195d4fa3a23de9b7690f5182f330b1
Justin Clark <jclarksummit@gmail.com>
2023-08-07 14:57:18 -0400

    Add container to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import Nav from 'src/components/navigation/Nav';
 import RoutesList from './RoutesList';
+import ContentArea from 'src/components/ContentArea';
 
 function App() {
 
   return (
     <div className="App">
       <Nav />
-      <RoutesList />
+      <ContentArea>
+        <RoutesList />
+      </ContentArea>
     </div>
   );
 }
