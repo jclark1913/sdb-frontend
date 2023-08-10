e636279fb181a50d8fcd269e9bd175a95a01fbdd
Justin Clark <jclarksummit@gmail.com>
2023-08-10 11:06:57 -0400

    Add dropdown menu for mass actions

--- src/components/collections/CollectionDetail.jsx
+++ src/components/collections/CollectionDetail.jsx
@@ -1,4 +1,5 @@
-import React, { useEffect, useState, useContext, useRef } from 'react';
+import React, { useEffect, useState, useContext, useRef, Fragment } from 'react';
+import { Disclosure, Menu, Transition } from '@headlessui/react';
 import { useParams } from 'react-router-dom';
 import SDBApi from 'src/api/api';
 import EntriesList from 'src/components/entries/EntriesList';
@@ -30,7 +31,7 @@ function CollectionDetail() {
 
   const handleSelectionChange = (selectedIds) => {
     selectedIdsRef.current = selectedIds;
-  }
+  };
 
   const handleActionWithSelectedIds = () => {
     console.log("We have selected the following ids: ", selectedIdsRef.current);
@@ -70,14 +71,74 @@ function CollectionDetail() {
               {isExpanded ? <ArrowsPointingInIcon className="stroke-white block h-6 w-6" /> : <ArrowsPointingOutIcon className="stroke-white block h-6 w-6" />}
             </button>
           </div>
-          <button
+          <Disclosure as="div" className="" >
+            <Menu as="div" className="">
+              <div>
+                <Menu.Button className="hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium border max-h-10">
+                  <span className="sr-only">Open additional actions</span>
+                  Show Additional Actions
+                  {/* <button
             onClick={handleActionWithSelectedIds}
             className="hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium border max-h-10">
-            Print to Excel
-          </button>
+            Additional Actions
+          </button> */}
+                </Menu.Button>
+              </div>
+              <Transition
+                as={Fragment}
+                enter="transition ease-out duration-100"
+                enterFrom="transform opacity-0 scale-95"
+                enterTo="transform opacity-100 scale-100"
+                leave="transition ease-in duration-75"
+                leaveFrom="transform opacity-100 scale-100"
+                leaveTo="transform opacity-0 scale-95"
+              >
+                <Menu.Items className="absolute z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                  <Menu.Item>
+                    {({ active }) => (
+                      <button
+                        className={`${active ? 'bg-gray-100' : ''} block px-4 py-2 text-sm text-gray-700 w-full text-left`}
+                        onClick={handleActionWithSelectedIds}>
+                        Print to excel
+                      </button>
+                    )}
+                  </Menu.Item>
+                  <Menu.Item>
+                    {({ active }) => (
+                      <button
+                        className={`${active ? 'bg-gray-100' : ''} block px-4 py-2 text-sm text-gray-700 w-full text-left`}
+                        onClick={handleActionWithSelectedIds}>
+                        Migrate selected
+                      </button>
+                    )}
+                  </Menu.Item>
+                  <Menu.Item>
+                    {({ active }) => (
+                      <button
+                        className={`${active ? 'bg-gray-100' : ''} block px-4 py-2 text-sm text-gray-700 w-full text-left`}
+                        onClick={handleActionWithSelectedIds}>
+                        Translate selected
+                      </button>
+                    )}
+                  </Menu.Item>
+                  <Menu.Item>
+                    {({ active }) => (
+                      <button
+                        className={`${active ? 'bg-gray-100' : ''} block px-4 py-2 text-sm text-gray-700 w-full text-left`}
+                        onClick={handleActionWithSelectedIds}>
+                        Summarize selected with AI
+                      </button>
+                    )}
+                  </Menu.Item>
+
+                </Menu.Items>
+
+              </Transition>
+            </Menu>
+          </Disclosure>
         </div>
       </div>
-      <EntriesList onSelectionChange={handleSelectionChange}entries={collection.entries} />
+      <EntriesList onSelectionChange={handleSelectionChange} entries={collection.entries} />
     </div>
   );
 
