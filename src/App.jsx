ac188d1fae866bbbe3d88bff9f8bef4fe0a0c4f2
Justin Clark <jclarksummit@gmail.com>
2023-08-11 15:55:30 -0400

    Add path as key for ContentArea

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import React, { useState, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 import Nav from 'src/components/navigation/Nav';
 import RoutesList from './RoutesList';
 import ContentArea from 'src/components/ContentArea';
 
 function App() {
 
+  const location = useLocation();
+
   return (
     <div className="App">
       <Nav />
-      <ContentArea>
+      <ContentArea key={location.pathname}>
         <RoutesList />
       </ContentArea>
     </div>
