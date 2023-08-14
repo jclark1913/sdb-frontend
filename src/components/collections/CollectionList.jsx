fbc990ce98649d735af6e882c42ec996fa63f3d8
Justin Clark <jclarksummit@gmail.com>
2023-08-14 16:47:52 -0400

    Add function to create collection + add state for displaying add form in modal

--- src/components/collections/CollectionList.jsx
+++ src/components/collections/CollectionList.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import CollectionCard from 'src/components/collections/CollectionCard';
 import SDBApi from 'src/api/api';
+import AddCollectionModal from './AddCollectionModal';
 
 /** CollectionList
  *
@@ -9,6 +11,7 @@ import SDBApi from 'src/api/api';
  *  State:
  *        - collections: [{id, name, description}, ...]
  *        - isLoading: boolean
+ *        - showModal: boolean
  *
  *
  * App -> ??? -> CollectionList -> CollectionCard
@@ -18,8 +21,13 @@ import SDBApi from 'src/api/api';
 function CollectionList() {
   const [collections, setCollections] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [showModal, setShowModal] = useState(false);
+
+  const navigate = useNavigate();
 
   console.log(collections, "Collections in CollectionList (state)");
+
+  useEffect;
 
   useEffect(function getCollectionsOnMount() {
     async function getCollections() {
@@ -32,9 +40,13 @@ function CollectionList() {
     }
   }, [isLoading]);
 
-  async function handleAddCollection() {
-
-  }
+  const handleAddCollection = async (name, description) => {
+    const data = {
+      name: name,
+      description: description
+    };
+    await SDBApi.addCollection(data);
+  };
 
   if (isLoading) {
     return <h1>Loading...</h1>;
@@ -45,7 +57,7 @@ function CollectionList() {
       <div className="mb-5 pb-5 border-b flex flex-1 justify-between">
         <h1 className='text-3xl font-medium'>Saved Collections</h1>
         <button
-          onClick={() => console.log("Add Collection button clicked")}
+          onClick={() => setShowModal(!showModal)}
           className="hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium border">+ Collection</button>
       </div>
       <div className="CollectionList-List grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-2">
@@ -60,6 +72,7 @@ function CollectionList() {
             />
           )) : null}
       </div>
+      <AddCollectionModal showModal={showModal} onClose={() => setShowModal(false)} onSubmit={handleAddCollection} />
     </div>
   );
 }
