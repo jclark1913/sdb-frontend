0b3f2a9f8683b77a983f133f2af8d95861c4fcd0
Justin Clark <jclarksummit@gmail.com>
2023-08-14 17:22:16 -0400

    Add delete button and handleDelete function

--- src/components/collections/CollectionCard.jsx
+++ src/components/collections/CollectionCard.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { TrashIcon } from '@heroicons/react/24/outline';
 
 /** Shows basic info about a given collection
  *
@@ -8,16 +9,28 @@ import { Link } from 'react-router-dom';
  * CollectionList -> CollectionCard
  */
 
-function CollectionCard({ id, name, description, createdAt }) {
+function CollectionCard({ id, name, description, createdAt, handleDelete }) {
+
+  const onDelete = async () => {
+    await handleDelete(id);
+  }
+
   return (
-    <div className="CollectionCard max-w-sm rounded shadow-sm border hover:bg-gray-700 hover:text-white" href={`/collections/${id}`}>
-      <a href={`/collections/${id}`}>
+    <div className="CollectionCard max-w-sm rounded shadow-sm border relative" href={`/collections/${id}`}>
+      <a className="hover:bg-gray-700 hover:text-white block" href={`/collections/${id}`}>
         <div className="card-body px-6 py-4 flex-col">
           <h5 className="text-xl font-medium pb-3">{name}</h5>
           <p className="card-text text-">{description}</p>
           <p className="card-text self-end">{createdAt}</p>
+
         </div>
       </a>
+          <TrashIcon onClick={(e) => {
+            e.stopPropagation();
+            onDelete();
+          }}
+            className="stroke-black block h-6 w-6 absolute top-3 right-3 cursor-pointer hover:bg-red-500 rounded-md border"
+          />
     </div>
   );
 }
