fefd3d44367fb60326d8ee832e603c5ec12b4900
Justin Clark <jclarksummit@gmail.com>
2023-08-24 17:57:11 -0400

    Add more scaffolding for ScrapeForm:
    - Add useEffect to get sources and collections on mount
    - Add submit button + dropdown for available collections

--- src/components/scrape/ScrapeForm.jsx
+++ src/components/scrape/ScrapeForm.jsx
@@ -1,15 +1,27 @@
 import React from "react";
 import Multi from "react-select";
-
-const sourceOptions = [
-    { value: "DEZ24", label: "Deir Ezzor 24" },
-    { value: "ENABBALADI", label: "Enab Baladi" },
-    { value: "HOURANFL", label: "Houran Free League" },
-    { value: "SANA", label: "Syrian Arab News Network (SANA)" },
-    { value: "SYRIADIRECT", label: "Syria Direct" },
-];
+import { useState, useEffect } from "react";
+import SDBApi from "src/api/api";
 
 const ScrapeForm = () => {
+    const [sources, setSources] = useState([]);
+    const [collections, setCollections] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(function getSourcesAndCollectionsOnMount() {
+        async function getSourcesAndCollections() {
+            const sources = await SDBApi.getSources();
+            const collections = await SDBApi.getCollections();
+            setSources(sources);
+            setCollections(collections);
+            setIsLoading(false);
+        }
+        if (isLoading) {
+            getSourcesAndCollections();
+        }
+    });
+
+
     return (
         <div className="ScrapeForm">
             <div className="mb-5 pb-5 border-b flex flex-1 justify-between">
@@ -35,8 +47,11 @@ const ScrapeForm = () => {
                             <label htmlFor="publications" className="block text-sm font-medium text-gray-700">Available publications:</label>
                             <div name="publications" className="bg-white flex border-red-500 p-5 border flex-col">
 
-
-                                {sourceOptions.map((source, idx) => (
+                                <div className="flex items-center mr-5">
+                                    <input id="all" name="all" type="checkbox" className="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300 rounded" />
+                                    <label htmlFor="all" className="ml-3 block text-sm text-gray-700 font-bold">Select all</label>
+                                </div>
+                                {sources.map((source, idx) => (
                                     <div key={idx} className="flex items-center mr-5">
                                         <input id={source.value} name={source.value} type="checkbox" className="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300 rounded" />
                                         <label htmlFor={source.value} className="ml-3 block text-sm font-light text-gray-700">{source.label}</label>
@@ -52,8 +67,18 @@ const ScrapeForm = () => {
                     <div className="w-1/2">
                         <h2 className="text-xl font-medium">Collection</h2>
                         <p>All data gathered will be added to the selected collection.</p>
+                        <select className="mt-5 text-lg">
+                            {collections.map((collection, idx) => (
+                                <option key={idx} value={collection.id}>{collection.name} -- {collection.created_at}</option>
+                            ))}
+                        </select>
                     </div>
                 </div>
+
+                <div className="flex justify-center m-5 pb-10 pt-10">
+                    <button className="bg-red-500 hover:bg-red-700 hover:text-white rounded-md px-3 py-2 text-xl font-medium border">Scrape</button>
+                </div>
+
 
 
 
